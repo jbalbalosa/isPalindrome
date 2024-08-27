95e0600a3d6693915828b91b25ab2ab75dbe9739
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-08-27 15:15:16 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("isPalindrome !");
+
+        System.out.println("The number is Palindrome: " + isPalindrome(-1221));
+        System.out.println("The number is Palindrome: " + isPalindrome(707));
+        System.out.println("The number is Palindrome: " + isPalindrome(11212));
+
+    }
+
+    public static boolean isPalindrome(int number){
+
+        int originalNumber = number;
+        int reversedNumber = 0;
+        int lastDigit = 0;
+
+        while(number != 0){
+            lastDigit = number % 10;
+            reversedNumber = reversedNumber * 10 +lastDigit;
+            number /= 10;
+        }
+
+        return (reversedNumber == originalNumber);
     }
 }
